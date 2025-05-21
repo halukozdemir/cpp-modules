24ad17d3050d0896774492af01bc26589062b0c7
halukozdemir <0halukozdemir@gmail.com>
2025-05-21 17:03:44 +0300

    cpp04 finished

---
+++ cpp04/ex01/Dog.cpp
@@ -0,0 +1,43 @@
+#include "Dog.hpp"
+#include <iostream>
+
+Dog::Dog() : Animal(), brain(new Brain())
+{
+    type = "Dog";
+    std::cout << "Dog constructed" << std::endl;  
+}
+
+Dog::Dog(const Dog& other) : Animal(other)
+{
+    brain = new Brain(*other.brain);
+    std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& other)
+{
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        if (brain)
+            delete brain;
+        brain = new Brain(*other.brain);
+    }
+    std::cout << "Dog assignment operator called" << std::endl;
+    return *this;
+}
+
+Dog::~Dog()
+{
+    delete brain;
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound() const
+{
+    std::cout << "Woof!" << std::endl;
+}
+
+Brain* Dog::getBrain() const
+{
+    return brain;
+}
