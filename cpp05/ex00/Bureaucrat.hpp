186cf18da5006a0bdf33d9f8d104e7103e5a5703
halukozdemir <0halukozdemir@gmail.com>
2025-06-02 02:31:10 +0300

    cpp05 started

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -4,5 +4,20 @@
 #include <iostream>
 
 class Bureaucrat
+{
+    private:
+        const std::string name;
+        int grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const Bureaucrat &other);
+        ~Bureaucrat();
+        Bureaucrat &operator=(const Bureaucrat &other);
+        std::string getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+}
 
 #endif
