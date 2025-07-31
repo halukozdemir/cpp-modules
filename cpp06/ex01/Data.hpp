d66fac2d30cd513e2246df7eb9c893cfa49e60dd
Haluk Mert Özdemir <halozdem@k1m18s06.42istanbul.com.tr>
2025-07-31 20:48:50 +0300

    cpp06 & cpp07/ex00 done

---
+++ cpp06/ex01/Data.hpp
@@ -0,0 +1,19 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <string>
+
+struct Data
+{
+    std::string s1;
+    int i;
+    std::string s2;
+    bool b;
+    char c;
+    float f;
+    double d;
+    short s;
+    long l;
+};
+
+#endif
